0d4b1060b541d57d016c7ca610b31b7e230966e7
Zoe <zpiccirillo10@stuy.edu>
2020-10-16 19:08:22 -0400

    update method shift_letter to use mode and encode/decode

--- Vigenere.py
+++ Vigenere.py
@@ -1,164 +1,50 @@
-str = "DVYCNZVJIHNVMNBDFTRRSNNXPIRMFMZZOYUFXGBEHKEVDDFVMTURWDAXMDGKMZBIOJZFOZLZOHLGVMFVBIQEPOUZOBCRSOVTVGNIUJVEUZEVTOZVPIFYPMRZUCBLHCGZXJHCENNZMVOFVONCJOGCFVAUTZRKIZJRUZEPQVEKPAGYFRBIMYVKJNNNBTVYBQRFGYEZWDAXPASKIZFGMZREBIQIFBHCBOVEHOUVDDETVGNKJJANIZAVWZEZGDAUNTFVMATIPRVEHBEZNVOFVOGYFHBLUCJYFIRMFMVKJNNUBHCUSDMQMTAFWZZSFMVENTFFVGJYFIRMFMVWJIQDZNRCGDAMPGHEUVEZMTCRVNVEHWRWPMRTPASZORNIFCBLTZFROYOIJITZOBHGUCRIFVEFGZIVSTSLOZERMDZVFONEEZFGFXVRMGLNIZAVWZEDZCLGPNTVUNHTIVALQKRIIVAUPAZVUCNKJOEVRPVIFNNJUMBEHHBIBGCIJIPZQGRKPKEVWZAKNZSIPHQVMDOVSVGVMTFKFKCZOBVEUJGYFNGIFZGROYZVUCBUJXNCMTXEPXXZOBCVPKYVTCNKTJSWUCREJVPTPPAKJOUZHCGZNZGFHZGKPNRRBNFFPINJJXNEUCVJJNZPTPOJUDGLUZSFSKVJUJYROYORMGJZUCNGIDYFTJCYJXNCGGBLSDFYDVGFUCEFXNUZNNRCGPCFOCVJTRBIEDDLJZGCZONBFOBKIZFYJKGYFMRZTIBKIDAXTPEGSDFZOBVEUCVJJAGYFTOLUFAVXDGRMHBJUVYCNZAZOOUVJMQVHMRVTJZVUDZVPMBKIZETIZEZTCIVSTAVBMYPUCRJBHRWFZYZOBFKPRNIENGYFJPVBIJZUCZVUCRIFIBNJNLFVMVETPYRSXVKZJSKIZZROCNKUJRJCZYKFYEFVIQSZRURSQRJBNVEEDNEJNYVTWLTPMNCSZRWTXBDNZETFNHISJHEENVKXDGYIZEJVMSIJBUKBIQCFAGKIZFKSZRKTONBFTBLXVGVSRNIEDGJFSGIFHRUPRAKPRAZTOUVCVGKFMLNIZEVUCNKOJOCFHBCFDFNBNUVEWLNBQRJBIQTPJYVEWLSSZRQFNJYJXURGZJYPPEJQMRMJJHJXZEVPPGFGNVXIOBWMVAUMJBBBOGYFXEFXYFFGRNKFMTRAZEJUCRIF"
+def shift_letter(letter, key_letter, algorithm):
+    letter = letter.lower()
+    key_letter = key_letter.lower()
+    alpha_index = {"a": 0, "b": 1, "c": 2,
+    "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9, "k": 10, "l": 11, "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18,
+                "t": 19, "u": 20, "v": 21, "w": 22, "x": 23, "y": 24, "z": 25}
 
+    letter_index = alpha_index[letter]
+    #print(letter_index)
+    key_letter_index = alpha_index[key_letter]
+    #print(key_letter_index)
+    shift_index = 0
+    if (algorithm == "decode"):
+        shift_index = letter_index - key_letter_index
+        if (shift_index < 0):
+            shift_index = shift_index % 26
+    else:
+        shift_index = letter_index + key_letter_index
+        if (shift_index > 25):
+            shift_index = shift_index % 26
+    alpha_keys = list(alpha_index.keys())
+    print(alpha_keys[shift_index])
 
-#string #1: first letters of key
-#slice_str0 = slice(0, -1, 4)
-#print(str[slice_str0])
-#DNIMFSPFOXHDMWMMOOOVBPOSVUUTPPUHXEMVJFTIUQPFMJBBGWPIMBFBHDVJIWGNMPHNVFUFFJBSMWFNVFFJZGPUMVHPPOFTOJOUFGSOMFEFMIWZPUIQIPUJRFUHBJQPWNPMSMFOUFFOUJMPOPTTUJPJHNHPBPJUJTUUSUOMUITJGSDUXNGOTEJZFIJFTITSOUJFUXMUNOJHTUPIITSBUBFOPEFBUUFJVTSZIOUCFVZSBEJTPSTNFSEXIVJBFISTFXSEFFPPTCFIUOFFBEBBPESFJGPQJXPGIMMBFXGFAU
 '''
-FREQUENCIES OF STRING #1:
+print("HOTDOGSTAND")
+shift_letter("I", "B", "decode")
+shift_letter("C", "O", "decode")
+shift_letter("T", "A", "decode")
+shift_letter("U", "R", "decode")
+shift_letter("P", "B", "decode")
+shift_letter("U", "O", "decode")
+shift_letter("S", "A", "decode")
+shift_letter("K", "R", "decode")
+shift_letter("B", "B", "decode")
+shift_letter("B", "O", "decode")
+shift_letter("D", "A", "decode")
 
-a :0.33% |*
-b :5.74% |**********************
-c :0.67% |**
-d :1.35% |*****
-e :3.04% |************
-f :12.5% |**************************************************
-g :3.04% |************
-h :3.04% |************
-i :5.74% |**********************
-j :7.77% |*******************************
-k :0.0%  |
-l :0.0%  |
-m :6.41% |*************************
-n :3.04% |************
-o :6.41% |*************************
-p :9.12% |************************************
-q :1.35% |*****
-r :0.33% |*
-s :5.4%  |*********************
-t :5.74% |**********************
-u :9.79% |***************************************
-v :3.37% |*************
-w :1.35% |*****
-x :2.7%  |**********
-y :0.0%  |
-z :1.68% |******
-
-key: bcdefghijklmnopqrstuvwxyza
+print("ICTUPUSKBBD")
+shift_letter("H", "B", "encode")
+shift_letter("O", "O", "encode")
+shift_letter("T", "A", "encode")
+shift_letter("D", "R", "encode")
+shift_letter("O", "B", "encode")
+shift_letter("G", "O", "encode")
+shift_letter("S", "A", "encode")
+shift_letter("T", "R", "encode")
+shift_letter("A", "B", "encode")
+shift_letter("N", "O", "encode")
+shift_letter("D", "A", "encode")
 '''
-#string #2: second letters of key
-#slice_str1 = slice(1, -2, 4)
-#print(str[slice_str1])
-#VZHNTNIMYGKDTDDZJZHMIOBOGJZOIMCCJNVOOVZZZVARYNTQYDAZZIBOODGJZZDTARBVOHCIMNHDTZMTGIMINDGVTNWMARCZYIBCVZTZDOZXGZZCNNVKVACOPNMHGIGKZZHDVTKBJNZYCXTXBKCJCVPOCZZNNIXCNPDZKJYGCDJXGDVCNNPCRDZOOZKMIDPDBCATFDHVZOMMJDMZZCTMCHZBRNJICCINMPXJZCJZYIRQNDNWMZXZNJNDZMBIAZZOTVRDSHRROVMZCJHDNWQIJWZNXZPMJZPNOVJOXYRMZC
-'''
-FREQUENCIES OF STRING #2:
-
-a :2.35% |*********
-b :3.03% |************
-c :7.74% |******************************
-d :7.4%  |*****************************
-e :0.0%  |
-f :0.33% |*
-g :3.36% |*************
-h :3.36% |*************
-i :5.38% |*********************
-j :5.72% |**********************
-k :2.35% |*********
-l :0.0%  |
-m :6.39% |*************************
-n :8.41% |*********************************
-o :6.06% |************************
-p :2.69% |**********
-q :1.01% |****
-r :3.36% |*************
-s :0.33% |*
-t :4.37% |*****************
-u :0.0%  |
-v :5.38% |*********************
-w :1.01% |****
-x :3.03% |************
-y :2.69% |**********
-z :14.14%|********************************************************
-
-key: fghijklmnopqrstuvwxyzabcde
-'''
-#string #3: third letters of key
-#slice_str2 = slice(2, -3, 4)
-#print(str[slice_str2])
-#YVNBRNRZUBEFUAGBZLLFQUCVNVEZFRBGHNONGARJEEGBVNVREASFRQHVUENAAEAFTVEOGBJRVNCMAZVFJRVQRAHECVRRSNBFOTHREISEZNFVLAELTHARAZNEVNBBCPREASQOGFCVGGGZBNXXCYNSRPAUGGGRFNNVZOGSVYOJNYCNBFGEURCVBDGNBFGRBAEFVVGOAGBYAUQRZZBEEIAYRRYFNGPJZRBLVYVSZNRYEQURVNYLNRBEHHVGESUQGFRNBGNGGRAAUGLENOBFULRQYLRJUJERHEGVBABGEFNTE
-'''
-FREQUENCIES OF STRING #3:
-a :6.55% |**************************
-b :7.21% |****************************
-c :2.62% |**********
-d :0.32% |*
-e :8.52% |**********************************
-f :5.57% |**********************
-g :8.85% |***********************************
-h :2.62% |**********
-i :0.65% |**
-j :2.29% |*********
-k :0.0%  |
-l :2.95% |***********
-m :0.32% |*
-n :8.85% |***********************************
-o :2.62% |**********
-p :0.98% |***
-q :2.62% |**********
-r :9.83% |***************************************
-s :2.62% |**********
-t :1.31% |*****
-u :3.93% |***************
-v :7.86% |*******************************
-w :2.62% |**********
-x :0.65% |**
-y :3.6%  |**************
-z :3.93% |***************
-
-key: nopqrstuvwxyzabcdefghijklm
-'''
-#string #4: fourth letters of key
-#slice_str3 = slice(3, -4, 4)
-#print(str[slice_str3])
-#CJVDRXMZFEVVRXKIFZGVEZRTIEVVYZLZCZFCCUKRPKYIKNYFZXKGEICEVTKNVZUVIEZFYLYMKUUQFSEFYMWDCMEZREWTZILRIZGIFVLRVEGRNVDGVTLIUVKVIJEIIZKVKIVVVKZEYIRVUCEZVVKWETKZZFKRFJEJPJLFJRRZGFYCLYFFZCFJILCBKYYZKXGZEJYLVRJCZVVVVVKTZVVPJWZKIYVZVINFERKKRKJKFSRJEEVTCWDTIEKYJIKCKKKBLVIJIUKZVKNVKCCNVNJTVSQYRYJMJVFXWUBYFFKRJ
-'''
-FREQUENCIES OF STRING #4:
-
-a :0.0%  |
-b :1.02% |****
-c :5.11% |********************
-d :1.36% |*****
-e :6.82% |***************************
-f :6.82% |***************************
-g :2.38% |*********
-h :0.0%  |
-i :7.16% |****************************
-j :6.14% |************************
-k :10.58%|******************************************
-l :3.41% |*************
-m :1.7%  |******
-n :2.38% |*********
-o :0.0%  |
-p :1.02% |****
-q :0.68% |**
-r :6.14% |************************
-s :1.02% |****
-t :3.07% |************
-u :2.73% |**********
-v :13.31%|*****************************************************
-w :0.68% |**
-x :1.7%  |******
-y :5.8%  |***********************
-z :8.87% |***********************************
-
-key: jklmnopqrstuvwxyzabcdefghi
-'''
-
-
-
-def decode(letter, key_letter):
-    key_index = {"j": 9, "k": 10, "l": 11, "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18,
-                "t": 19, "u": 20, "v": 21, "w": 22, "x": 23, "y": 24, "z": 25, "a": 0, "b": 1, "c": 2,
-                "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8}
-
-
-    alpha_index = {0: "a", 1: "b",  2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h", 8: "i", 9: "j", 10: "k", 11: "l", 12: "m",
-                 13: "n", 14: "o", 15: "p", 16: "q", 17: "r", 18: "s", 19: "t", 20: "u", 21: "v", 22: "w", 23: "x", 24: "y", 25: "z"}
-
-    letter_index = key_index[letter]
-    key_letter_index = key_index[key_letter]
-    decode_index = letter_index - key_letter_index
-    if (decode_index < 0):
-        decode_index = decode_index + 26
-    print(alpha_index[decode_index])
