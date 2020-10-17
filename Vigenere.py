38495c91d43d5318f2066435277af8b2e1f802ca
Zoe <zpiccirillo10@stuy.edu>
2020-10-16 20:59:45 -0400

    add documentation

--- Vigenere.py
+++ Vigenere.py
@@ -1,15 +1,24 @@
+'''
+Zoe Piccirillo
+Cybersecurity Pd. 5
+October 19, 2020
+
+Encode/decode Vigenere ciphers using a keyword
+
+Encode: make run ARGS="encode plaintext key"
+Decode: make run ARGS="decode ciphertext key"
+'''
 import sys
 
+#modify the keyword so it matches the length of the text, go through the keyword & text letter by letter to encode/decode
 def solve(text, key_word, algorithm):
-    #add keyword to itself until it reaches the length of the text
-    #note to self: change everything to uppercase later
     #note to self: do we have to account for one-letter keys too?? (autokey method?)
+    #remove punctuation and whitespace from text
     punc = '''!()-[]{};:'"\, <>./?@#$%^&*_~'' " "'''
-
     for letter in text:
         if letter in punc:
             text = text.replace(letter, "")
-
+    #add keyword to itself until it reaches the length of the text
     key = key_word
     count = 0
     while (len(key) < len(text)):
@@ -18,34 +27,34 @@ def solve(text, key_word, algorithm):
             count = 0
         else:
             count += 1
+    #decode the text using the keyword, letter by letter
     str = ""
     for i in range(len(text)):
         str += shift_letter(text[i], key[i], algorithm)
     print(str.upper())
 
+#shift individual letters based on letter & corresponding key letter
 def shift_letter(letter, key_letter, algorithm):
     letter = letter.lower()
     key_letter = key_letter.lower()
-    alpha_index = {"a": 0, "b": 1, "c": 2,
-    "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9, "k": 10, "l": 11, "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18,
-                "t": 19, "u": 20, "v": 21, "w": 22, "x": 23, "y": 24, "z": 25}
-
+    alpha_index = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, #dictionary to store letters and their indexes
+                "i": 8, "j": 9, "k": 10, "l": 11, "m": 12, "n": 13, "o": 14, "p": 15,
+                "q": 16, "r": 17, "s": 18, "t": 19, "u": 20, "v": 21, "w": 22, "x": 23, "y": 24, "z": 25}
     letter_index = alpha_index[letter]
-    #print(letter_index)
     key_letter_index = alpha_index[key_letter]
-    #print(key_letter_index)
     shift_index = 0
-    if (algorithm == "decode"):
+    if (algorithm == "decode"): #for decoding: subtract key letter index from letter index & adjust if less than 0
         shift_index = letter_index - key_letter_index
         if (shift_index < 0):
             shift_index = shift_index % 26
-    else:
+    else: #for encoding: subtract key letter index from letter index & adjust if less than 0
         shift_index = letter_index + key_letter_index
         if (shift_index > 25):
             shift_index = shift_index % 26
-    alpha_keys = list(alpha_index.keys())
-    return alpha_keys[shift_index]
+    alpha_keys = list(alpha_index.keys()) #index of encoded/decoded letter
+    return alpha_keys[shift_index] #return the encoded/decoded letter
 
+#main method: take arguments algorith, text and key_word; solve
 if __name__ == "__main__":
     algorithm = sys.argv[1]
     text = sys.argv[2]
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 make:
-	@echo To encode: \"make encode plaintext key\"
-	@echo To decode: \"make decode ciphertext key\"
+	@echo To encode: make run ARGS=\"encode plaintext key\"
+	@echo To decode: make run ARGS=\"decode ciphertext key\"
 
 run:
 	python3 Vigenere.py $(ARGS)
